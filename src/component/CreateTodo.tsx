98b99ecdd50c7d2dbd7e0b8ccabcb2e5decbf4b5
Jordan Needham <jdncreations@hotmail.com>
2023-08-10 19:59:22 +0800

    optimistic updates

--- src/component/CreateTodo.tsx
+++ src/component/CreateTodo.tsx
@@ -9,6 +9,35 @@ export default function CreateTodo() {
   const trpc = api.useContext();
 
   const { mutate } = api.todo.create.useMutation({
+    onMutate: async (newTodo) => {
+      // canel any outgoing refetches so they don't overwrite our optimistic update
+      await trpc.todo.all.cancel();
+
+      // snapshot previous value
+      const previousTodos = trpc.todo.all.getData();
+
+      // optimistically update to the new value
+      trpc.todo.all.setData(undefined, (prev) => {
+        const optimisticTodo = {
+          id: "optimistic",
+          text: newTodo,
+          done: false,
+        };
+        if (!prev) return [optimisticTodo];
+        return [...prev, optimisticTodo];
+      });
+
+      setNewTodo("");
+
+      return { previousTodos };
+    },
+    onError: (err, newTodo, context) => {
+      toast.error("An error occurred when creating todo");
+      // bring back todo into textbox
+      setNewTodo(newTodo);
+      // set todos to previous data
+      trpc.todo.all.setData(undefined, () => context?.previousTodos);
+    },
     // invalidate queries, re-fetch data
     onSettled: async () => {
       await trpc.todo.all.invalidate();
--- src/component/Todo.tsx
+++ src/component/Todo.tsx
@@ -1,5 +1,6 @@
 import { api } from "~/utils/api";
 import type { Todo } from "../types";
+import { toast } from "react-hot-toast";
 
 type TodoProps = {
   todo: Todo;
@@ -11,12 +12,67 @@ export default function Todo({ todo }: TodoProps) {
   const trpc = api.useContext();
 
   const { mutate: doneMutation } = api.todo.toggle.useMutation({
+    onMutate: async ({ id, done }) => {
+      // canel any outgoing refetches so they don't overwrite our optimistic update
+      await trpc.todo.all.cancel();
+
+      // snapshot previous value
+      const previousTodos = trpc.todo.all.getData();
+
+      // optimistically update to the new value
+      trpc.todo.all.setData(undefined, (prev) => {
+        if (!prev) return previousTodos;
+        return prev.map((t) => {
+          if (t.id === id) {
+            return {
+              ...t,
+              done,
+            };
+          }
+          return t;
+        });
+      });
+
+      return { previousTodos };
+    },
+    onSuccess: (err, { done }) => {
+      if (done) {
+        toast.success("Todo completed! 🥳");
+      }
+    },
+    onError: (err, newTodo, context) => {
+      toast.error(
+        `An error occurred when setting todo to ${done ? "done" : "undone"}`
+      );
+      // set todos to previous data
+      trpc.todo.all.setData(undefined, () => context?.previousTodos);
+    },
     onSettled: async () => {
       await trpc.todo.all.invalidate();
     },
   });
 
   const { mutate: deleteMutation } = api.todo.delete.useMutation({
+    onMutate: async (deleteId) => {
+      // canel any outgoing refetches so they don't overwrite our optimistic update
+      await trpc.todo.all.cancel();
+
+      // snapshot previous value
+      const previousTodos = trpc.todo.all.getData();
+
+      // optimistically update to the new value
+      trpc.todo.all.setData(undefined, (prev) => {
+        if (!prev) return previousTodos;
+        return prev.filter((t) => t.id !== deleteId);
+      });
+
+      return { previousTodos };
+    },
+    onError: (err, newTodo, context) => {
+      toast.error("An error occurred when deleting todo");
+      // set todos to previous data
+      trpc.todo.all.setData(undefined, () => context?.previousTodos);
+    },
     onSettled: async () => {
       await trpc.todo.all.invalidate();
     },
@@ -29,14 +85,17 @@ export default function Todo({ todo }: TodoProps) {
           <input
             type="checkbox"
             name="done"
-            id="done"
+            id={id}
             className="h-4 w-4 cursor-pointer rounded border border-gray-300 bg-gray-50 "
             checked={done}
             onChange={(e) => {
               doneMutation({ id, done: e.target.checked });
             }}
           />
-          <label htmlFor="done" className={`cursor-pointer`}>
+          <label
+            htmlFor={id}
+            className={`cursor-pointer ${done ? "line-through" : ""}`}
+          >
             {text}
           </label>
         </div>
